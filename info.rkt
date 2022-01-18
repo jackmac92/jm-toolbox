d38dd5bdc95cb95fb2d84faa951c146ae94001a9
Jack McCown <jackmac79@gmail.com>
2022-01-17 19:26:28 -0500

    add deta for db

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "jm-toolbox")
-(define deps '("base" "behavior" "casemate" "ulid" "anaphoric" "threading" "reprovide-lang-lib"))
+(define deps '("base" "deta" "behavior" "casemate" "ulid" "anaphoric" "threading" "reprovide-lang-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/jm-toolbox.scrbl" ())))
 (define pkg-desc "Description Here")
