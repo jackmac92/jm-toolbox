911ea79fea1a00ba4e5c76525528e7f8810f1dff
Jack McCown <jackmac79@gmail.com>
2022-03-07 08:00:00 -0500

    feat: update email notifier

--- email-notifier.rkt
+++ email-notifier.rkt
@@ -1,18 +1,26 @@
 #lang racket/base
 
-(require racket/math "./notmuch.rkt" "./notify.rkt" "./list-utils.rkt" "./shellpers.rkt")
+(require
+ racket/system
+ racket/math
+ racket/port
+ racket/string
+ racket/list
+ "./notmuch.rkt" "./notify.rkt" "./list-utils.rkt" "./shellpers.rkt")
 
 (define accts '("cbi" "jackmccown7" "pitchapp" "personalj79" "colgate"))
 
 (define (generate-periodic-email-notifs acct id color [last-check-email-count 0])
-  (define email-count (notmuch-count-unread acct))
+  (define email-infos (notmuch-list-unread acct))
+  (define email-count (length email-infos))
+  (define email-ids (for/list ([e email-infos])
+                      (last (string-split e))))
   (displayln (format "~a has ~a new messages" acct email-count))
   (define new-email-count (- email-count last-check-email-count))
-  (when ( > email-count  0)
-    ;; 1 is timeout
-    ;; 2 is right click
+  (when ( > new-email-count 0)
     (thread (lambda ()
               (let ((response (dunstify (stringify-flag-pairs (list
+                                                               (cons "--timeout" (* 1000 60 20))
                                                                (cons "--icon" "/usr/share/icons/HighContrast/scalable/apps-extra/internet-mail.svg")
                                                                (cons "--hints" "string:category:email.arrived")
                                                                (cons "--hints" (format "string:bgcolor:~a" color))
@@ -21,17 +29,25 @@
                                                                (cons "--action" (format "~s" "default,Open Notmuch"))))
                                         #:title (format "~a received email" acct)
                                         #:subtitle (format "~a new emails" email-count))))
-                (displayln (format "Notif action ~a" response))))))
-  (sleep 5)
+                (case (string-trim response)
+                  [("1") (begin (displayln "Handling timeout"))]
+                  [("2") (begin (displayln "Handling dismissed"))]
+                  [("default") (begin
+                                 (displayln "Handling default action")
+                                 (define email-ids-as-stdin (string-join email-ids "\n"))
+                                 (displayln email-ids-as-stdin)
+                                 (with-input-from-string email-ids-as-stdin
+                                   (lambda ()
+                                     (system "/home/jmccown/.local/sysspecific_scripts/gui/emacs-view-emails"))))])))))
+  (sleep 15)
   (generate-periodic-email-notifs acct id color email-count))
+
+(define colors (shuffle '("#ff71ce" "#01cdfe" "#05ffa1" "#b967ff" "#fffb96")))
 
 (define (start-email-notifier acct)
   (define id (exact-floor (* 20000 (random))))
-  (define color (pick '("#ff71ce" "#01cdfe" "#05ffa1" "#b967ff" "#fffb96")))
+  (define color (list-ref colors (index-of accts acct)))
   (thread (lambda () (generate-periodic-email-notifs acct id color))))
 
-(for ([t (for/list ([acct accts])
-            (start-email-notifier acct))])
+(for ([t (for/list ([acct accts]) (start-email-notifier acct))])
   (thread-wait t))
-
-;; "$addr received email" "$msgcount new emails" | rg -q launchEmail ; then echo "$newmessages" | s gui emacs-view-emails ; fi &
--- notmuch.rkt
+++ notmuch.rkt
@@ -5,19 +5,25 @@
 (require racket/port)
 (require racket/string)
 
-(provide notmuch-count-unread notmuch-dump-message-ids)
+(provide (all-defined-out))
 
 (define (notmuch-dump q)
   (for/list ([line (string-split (with-output-to-string (lambda () (system (format "notmuch dump ~s" q)))) "\n")]
              #:when (not (string-prefix? line "#notmuch-dump")))
       line))
 
-(define (notmuch-dump-message-ids q)
-  (for/list ([line (notmuch-dump q)])
+(define (notmuch-dump-extract-message-ids resp)
+  (for/list ([line resp])
     (last (string-split line))))
 
+(define (notmuch-dump-message-ids q)
+  (notmuch-dump-extract-message-ids (notmuch-dump q)))
+
+(define (notmuch-list-unread acct)
+  (notmuch-dump (format "is:unread AND is:inbox AND tag:~a" acct)))
+
 (define (notmuch-count-unread acct)
-  (length (notmuch-dump (format "is:unread AND is:inbox AND tag:~a" acct))))
+  (length (notmuch-list-unread acct)))
 
 
 (module+ test
