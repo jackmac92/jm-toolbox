d241bc50fbf7cc6519f9df5a79f566e8a6e4f3d0
Jack McCown <jackmac79@gmail.com>
2022-03-31 20:17:43 -0400

    feat: slack_status replacement

---
+++ slack.rkt
@@ -0,0 +1,30 @@
+#lang racket/base
+
+(require racket/cmdline net/http-easy)
+
+
+(define (slack-api-req path body)
+  (post (format "https://slack.com/api/~a" path)
+        #:auth (bearer-auth (getenv "SLACK_API_TOKEN"))
+        #:json body))
+
+;; (slack-api-req "users.profile.set" (hasheq 'profile "PROFILE_STUFF"))
+
+;; (post "https://slack.com/api/users.profile.set"
+;;       #:form (list (cons 'profile "PROFILE_STUFF") (cons 'token "TOKEN")))
+;; PROFILE="{\"status_emoji\":\"$EMOJI\",\"status_text\":\"$TEXT\"}"
+;; RESPONSE=$(curl -s --data token="$TOKEN" \
+;;     --data-urlencode profile="$PROFILE" \
+;;     https://slack.com/api/users.profile.set)
+;; let profile = json!({
+;;                      "status_emoji": status.emoji,
+;;                      "status_text": status.text,
+;;                      "status_expiration": expiration})
+;;     ;
+
+(command-line
+  #:program "slacker"
+  #:args (action)
+  (cond
+    [(string=? action "status") (slack-api-req "users.profile.set" (hasheq 'profile (hasheq 'status_emoji ":hellmo:"
+                                                                                            'status_text "test")))]))
