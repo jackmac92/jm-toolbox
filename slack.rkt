2f8dccdfefa784b2a39ee388946f37d760ba5b12
Jack McCown <jackmac79@gmail.com>
2022-04-20 21:57:02 -0400

    reformat

--- slack.rkt
+++ slack.rkt
@@ -3,7 +3,6 @@
 (require racket/cmdline
          net/http-easy)
 
-
 (define (slack-api-req path body)
   (post (format "https://slack.com/api/~a" path)
         #:auth (bearer-auth (getenv "SLACK_API_TOKEN"))
@@ -11,30 +10,37 @@
 
 (define forty-five-min (* 60 45))
 
-(define presets (hasheq
-                 'reset (hasheq 'status_emoji ""
-                                'status_expiration 0
-                                'status_text "")
-                 'taz (hasheq 'status_emoji ":jump:"
-                              'status_expiration (floor (+ (current-seconds) forty-five-min))
-                              'status_text "walking Taz")
-                 'zoom (hasheq 'status_emoji ":zoom:"
-                               'status_expiration 0
-                               'status_text "In a zoom meeting")
-                 'lunch (hasheq 'status_emoji ":chompy:"
-                                'status_expiration (floor (+ (current-seconds) forty-five-min))
-                                'status_text "eating")
-                 'testing (hasheq 'status_emoji ":hellmo:"
-                                  'status_expiration (floor (+ (current-seconds) forty-five-min))
-                                  'status_text "test")))
+(define presets
+  (hasheq 'reset
+          (hasheq 'status_emoji "" 'status_expiration 0 'status_text "")
+          'taz
+          (hasheq 'status_emoji
+                  ":jump:"
+                  'status_expiration
+                  (floor (+ (current-seconds) forty-five-min))
+                  'status_text
+                  "walking Taz")
+          'zoom
+          (hasheq 'status_emoji ":zoom:" 'status_expiration 0 'status_text "In a zoom meeting")
+          'lunch
+          (hasheq 'status_emoji
+                  ":chompy:"
+                  'status_expiration
+                  (floor (+ (current-seconds) forty-five-min))
+                  'status_text
+                  "eating")
+          'testing
+          (hasheq 'status_emoji
+                  ":hellmo:"
+                  'status_expiration
+                  (floor (+ (current-seconds) forty-five-min))
+                  'status_text
+                  "test")))
 
-(command-line
- #:program "slacker"
- #:args (action [subcommand #f])
- (cond
-   [(string=? action "status") (slack-api-req
-                                "users.profile.set"
-                                (hasheq 'profile
-                                        (hash-ref
-                                         presets
-                                         (string->symbol (or subcommand "reset")))))]))
+(command-line #:program "slacker"
+              #:args (action [subcommand #f])
+              (cond
+                [(string=? action "status")
+                 (slack-api-req
+                  "users.profile.set"
+                  (hasheq 'profile (hash-ref presets (string->symbol (or subcommand "reset")))))]))
