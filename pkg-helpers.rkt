529f3282a66612d808076b96d58be3fe5ada933a
Jack McCown <jackmac79@gmail.com>
2022-06-05 21:54:37 -0400

    add wip

---
+++ pkg-helpers.rkt
@@ -0,0 +1,11 @@
+#lang racket/base
+
+
+(provide (all-defined-out) (for-syntax (all-defined-out)))
+
+;; (require setup/getinfo)
+;; compile time
+;; (define-syntax (pkg-name!)
+;;   ((get-info/full ".") 'collection))
+
+
