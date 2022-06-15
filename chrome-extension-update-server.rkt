78be21ba9ac1f6b278558caeddb48a15128480e1
Jack McCown <jackmac79@gmail.com>
2022-06-15 08:00:00 -0400

    almost working chrome ext update server

--- chrome-extension-update-server.rkt
+++ chrome-extension-update-server.rkt
@@ -1,20 +1,31 @@
 #lang at-exp racket/base
 
-(require xml)
+(require
+ racket/format
+  xml)
 (require web-server/servlet)
 (require web-server/servlet-env)
+(require "./gitlab-api.rkt")
 
-(define ( create-xml url id ver) @~a{
-                                     <?xml version='1.0' encoding='UTF-8'?>
-                                     <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
-                                     <app appid='@id'>
-                                         <updatecheck codebase='@url' version='@ver' />
-                                     </app>
-                                     </gupdate>})
+;; TODO codebase url should be a gitlab artifact
+;; make gitlab request to check the version, should run on just the name
+(define (create-xml url id ver) @~a{<?xml version='1.0' encoding='UTF-8'?>
+                                    <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
+                                    <app appid='@id'>
+                                        <updatecheck codebase='@url' version='@ver' />
+                                    </app>
+                                    </gupdate>})
+;; "https://gitlab.com/api/v4/projects/36836128/packages/generic/web-extension/0.0.1/chrome-dist.zip"
+
+(define (create-xml-dwim name chrome-ext-id)
+  (define latest-version (hash-ref (car (gitlab-project-request (format "jackmac92/~a" name) "releases")) 'tag_name))
+  (define url (format "~a/~a/~a" (gitlab-host) (gitlab-project-api-url (format "jackmac92/~a" name)) (format "packages/generic/web-extension/~a/chrome-dist.zip" latest-version)))
+  (create-xml url chrome-ext-id latest-version))
 
 (define (request-handler req)
-  (define ext (hash-ref (bindings-assq #"payload" (request-bindings/raw req)) 'name))
-  (xexpr->xml (hasheq 'hello "world")))
+  (define ext (binding:form-value (bindings-assq #"name" (request-bindings/raw req))))
+  (define ext-id (binding:form-value (bindings-assq #"chrome-id" (request-bindings/raw req))))
+  (response/output (lambda (op) (write-bytes (string->bytes/utf-8 (create-xml-dwim ext ext-id)) op))))
 
 (module+ main
   (serve/servlet request-handler
--- gitlab-api.rkt
+++ gitlab-api.rkt
@@ -1,13 +1,24 @@
-#lang racket/base
+#lang debug racket/base
+(require debug/repl)
+
 
 (require net/http-easy
-         "./mini-project-utils.rkt")
+         net/uri-codec)
 
-(define gitlab-api-token (make-parameter ""))
+(define gitlab-api-token (make-parameter (getenv "LAB_CORE_TOKEN")))
 (define gitlab-host (make-parameter "gitlab.com"))
+
+(define (gitlab-api-url)
+  (format "https://~a/api/v4/" (gitlab-host)))
 
 (define (gitlab-request path)
   (response-json (get (format "https://~a/api/v4/~a" (gitlab-host) path)
-                      #:headers (hasheq "Private-Token" (gitlab-api-token)))))
+                      #:headers (hasheq (string->symbol "Private-Token") (gitlab-api-token)))))
+
+(define (gitlab-project-api-url proj-name)
+  (format "projects/~a" (uri-encode proj-name)))
+
+(define (gitlab-project-request project-id path)
+  (gitlab-request (format "~a/~a" (gitlab-project-api-url project-id) path)))
 
 (provide (all-defined-out))
