37b1c75d2b5e39bbcbeb6c4cd5ecfc6181aa66eb
Jack McCown <jackmac79@gmail.com>
2022-06-15 23:57:46 -0400

    chrome ext web server working

--- chrome-extension-update-server.rkt
+++ chrome-extension-update-server.rkt
@@ -7,24 +7,25 @@
 (require web-server/servlet-env)
 (require "./gitlab-api.rkt")
 
-;; TODO codebase url should be a gitlab artifact
-;; make gitlab request to check the version, should run on just the name
-(define (create-xml url id ver) @~a{<?xml version='1.0' encoding='UTF-8'?>
-                                    <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
-                                    <app appid='@id'>
-                                        <updatecheck codebase='@url' version='@ver' />
-                                    </app>
-                                    </gupdate>})
-;; "https://gitlab.com/api/v4/projects/36836128/packages/generic/web-extension/0.0.1/chrome-dist.zip"
+(define (print-xml url id ver) @~a{<?xml version='1.0' encoding='UTF-8'?>
+                                   <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
+                                   <app appid='@id'>
+                                       <updatecheck codebase='@url' version='@ver' />
+                                   </app>
+                                   </gupdate>})
 
 (define (create-xml-dwim name chrome-ext-id)
-  (define latest-version (hash-ref (car (gitlab-project-request (format "jackmac92/~a" name) "releases")) 'tag_name))
-  (define url (format "~a/~a/~a" (gitlab-host) (gitlab-project-api-url (format "jackmac92/~a" name)) (format "packages/generic/web-extension/~a/chrome-dist.zip" latest-version)))
-  (create-xml url chrome-ext-id latest-version))
+  (define latest-release (car (gitlab-project-request (format "jackmac92/~a" name) "releases")))
+  (define latest-version (hash-ref latest-release 'tag_name))
+  (define latest-release-details (gitlab-project-request (format "jackmac92/~a" name) (format "releases/~a" latest-version)))
+  (define url (for/first ([l (hash-ref (hash-ref latest-release-details 'assets) 'links)]
+                          #:when (equal? (hash-ref l 'name) "web-ext-dist.crx"))
+                (hash-ref l 'url)))
+  (print-xml url chrome-ext-id latest-version))
 
 (define (request-handler req)
   (define ext (binding:form-value (bindings-assq #"name" (request-bindings/raw req))))
-  (define ext-id (binding:form-value (bindings-assq #"chrome-id" (request-bindings/raw req))))
+  (define ext-id (binding:form-value (bindings-assq #"id" (request-bindings/raw req))))
   (response/output (lambda (op) (write-bytes (string->bytes/utf-8 (create-xml-dwim ext ext-id)) op))))
 
 (module+ main
