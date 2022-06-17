bcf25ab4cd2db94b368fed7804209a7fdd21a94c
Jack McCown <jackmac79@gmail.com>
2022-06-17 19:30:47 -0400

    upgrade

--- chrome-extension-update-server.rkt
+++ chrome-extension-update-server.rkt
@@ -1,10 +1,8 @@
 #lang at-exp racket/base
 
-(require
- racket/format)
 (require web-server/servlet)
 (require web-server/servlet-env)
-(require "./gitlab-api.rkt")
+(require "./mini-project-utils.rkt" "./gitlab-api.rkt")
 
 (define (print-xml url id ver) @~a{<?xml version='1.0' encoding='UTF-8'?>
                                    <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
@@ -14,8 +12,10 @@
                                    </gupdate>})
 
 (define (create-xml-dwim name chrome-ext-id)
+  (log-debug (format "creating xml on request for ~a" name))
   (define latest-release (car (gitlab-project-request (format "jackmac92/~a" name) "releases")))
   (define latest-version (hash-ref latest-release 'tag_name))
+  (log-debug (format "~a at version ~a" name latest-version))
   (define latest-release-details (gitlab-project-request (format "jackmac92/~a" name) (format "releases/~a" latest-version)))
   (define url (for/first ([l (hash-ref (hash-ref latest-release-details 'assets) 'links)]
                           #:when (equal? (hash-ref l 'name) "web-ext-dist.crx"))
@@ -31,18 +31,25 @@
 (define-values (dispatch _generate-url)
   (dispatch-rules
    [("info") lookup-ext-xml]
-   [("personal-super-cookie") (lambda (res) (response/output (lambda (op) (write-string (create-xml-dwim "personal-super-cookie" "odlameecjipmbmbejkplpemijjgpljce") op))))]
+   [("personal-super-cookie") (lambda (_req) (response/output (lambda (op) (write-string (create-xml-dwim "personal-super-cookie" "jefngaghghialgfnbdmcaminogidhpka") op))))]
    [("health") (lambda (_) (response/empty))]
    [else (error "There is no procedure to handle the url.")]))
 
 (define (request-handler request)
   (dispatch request))
 
-(module+ main
+(define (start-server)
   (serve/servlet request-handler
-                 #:launch-browser? #f
-                 #:quit? #t
-                 #:stateless? #t
-                 #:listen-ip "0.0.0.0"
-                 #:port 5576
-                 #:servlet-regexp #rx""))
+               #:launch-browser? #f
+               #:quit? #t
+               #:stateless? #t
+               #:listen-ip "0.0.0.0"
+               #:port 5576
+               #:servlet-regexp #rx""))
+(define (init)
+  (parameterize ([current-basedir-program-name "chrome-extension-server"])
+    (define logging-output-port (make-log-file (writable-runtime-file "out.log")))
+    (with-logging-to-port logging-output-port start-server 'debug)))
+
+(module+ main
+  (init))
--- mini-project-utils.rkt
+++ mini-project-utils.rkt
@@ -1,13 +1,16 @@
 #lang racket/base
 
-(require
- racket/file
- racket/port)
+(require reprovide/reprovide)
+
+(reprovide racket/format racket/file racket/list racket/string racket/logging racket/port basedir)
 
 (define make-log-file
   (lambda components
     (let ([logfilepath (apply build-path components)])
       (make-parent-directory* logfilepath)
+      ;; TODO use (make-pipe) to create a pipe which also logs to stdout before the logfile
+      ;; or maybe dup-output-port
+      ;;or maybe start a thread, where copy-port streams to current-output-port?
       (open-output-file logfilepath #:exists 'append))))
 
 (provide (all-defined-out))
