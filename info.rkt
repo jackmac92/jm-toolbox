6a042a84068b1176ca3682290862669022bb1d7e
Jack McCown <jackmac79@gmail.com>
2022-06-24 20:44:46 -0400

    bump version

--- info.rkt
+++ info.rkt
@@ -33,6 +33,6 @@
 (define build-deps '("scribble-lib" "racket-doc" "at-exp-lib" "rackjure"))
 (define scribblings '(("scribblings/jm-toolbox.scrbl" ())))
 (define pkg-desc "Description Here")
-(define version "0.0")
+(define version "1000.0")
 (define pkg-authors '(jmccown))
 (define license '(Apache-2.0 OR MIT))
