816bb386c0f07879f887129d80fce26dfc4a4d22
Jack McCown <jackmac79@gmail.com>
2022-06-24 20:39:18 -0400

    fix comment syntax

--- kv-store.rkt
+++ kv-store.rkt
@@ -1,4 +1,4 @@
 #lang racket/base
 
-# TODO some way for a persistent kv storage
-# first use case is for storing gitlab pipeline results
+;; # TODO some way for a persistent kv storage
+;; # first use case is for storing gitlab pipeline results
