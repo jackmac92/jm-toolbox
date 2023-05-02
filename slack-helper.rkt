dfd9d9742655574d3368d322a374e3dccaa6a841
Jack McCown <jackmac79@gmail.com>
2023-05-02 11:58:12 -0400

    self fetch slack token

--- slack-helper.rkt
+++ slack-helper.rkt
@@ -2,21 +2,24 @@
 
 (require json
          racket/cmdline
+         "./shellpers.rkt"
          net/http-easy)
 
 (define (slack-resp-wrapper resp)
   (write-json
    (response-json resp)))
 
+(define slack-token (or (getenv "SLACK_API_TOKEN") (system->string "s bitwarden get-field 'CBI Slack' 'status-script-token'")))
+
 (define (slack-api-post path body)
   (slack-resp-wrapper (post (format "https://slack.com/api/~a" path)
-                            #:auth (bearer-auth (getenv "SLACK_API_TOKEN"))
+                            #:auth (bearer-auth slack-token)
                             #:json body)))
 
 (define (slack-api-get path [params (list)])
   (slack-resp-wrapper (get (format "https://slack.com/api/~a" path)
                            #:params params
-                           #:auth (bearer-auth (getenv "SLACK_API_TOKEN")))))
+                           #:auth (bearer-auth slack-token))))
 
 (define forty-five-min (* 60 45))
 
